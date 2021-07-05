047f892aba7ba8ab08fa161927402b027b16c445
Reynaldo Teixeira Jr <reynaldoteixeira@gmail.com>
2021-07-05 19:36:04 -0300

    Usando request

--- src/client/ClientTwo.js
+++ src/client/ClientTwo.js
@@ -1,25 +1,41 @@
-const { default: axios } = require("axios")
+// const { default: axios } = require("axios")
+const request = require("request");
 
 async function createValuesClientTwo(menuItemValue, menuItemOnClick) {
-    
+   
+    let bodyMenu = {
+        "value":menuItemValue,
+        "onclick":menuItemOnClick
+    }
+
     var options = {
         "method": "POST",
         "url": "http://localhost:3333/menuitem",
-
-        data: {
-            "value":menuItemValue,
-            "onclick":menuItemOnClick
-        }
+        'headers': {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify(bodyMenu)
     }
 
-    
+    // try{
+    //     const res = await axios(options)
+    //     return res.data;
+    // }catch(error){
+    //     logger.error("Post client two failed");
+    // }
 
-    try{
-        const res = await axios(options)
-        return res.data;
-    }catch(error){
-        logger.error("Post client two failed");
-    }
+    return new Promise((resolve, reject) =>{
+
+        request (options, function(error, response, body){
+
+            if (error) {
+                reject(new Error(error));
+            }
+
+            resolve(body);
+        });
+
+    });
 }
 
 module.exports = {
